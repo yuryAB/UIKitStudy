b7736b100049ff4fed8b9c45fcff8908ae8273e9
yury <yury.antonyb@gmail.com>
2020-08-09 14:49:34 -0400

    Initial Commit

---
+++ UIKitStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitStudy
+//
+//  Created by yury antony on 09/08/20.
+//  Copyright © 2020 yuryAntony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
